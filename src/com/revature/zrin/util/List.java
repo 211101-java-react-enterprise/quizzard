db258f33d55f77e961a0d48e11edddb63a6eb9d9
Heather Zrinsky <zrinsky@VIIchariot.com>
2021-11-07 14:27:44 -0600

    Creating my personal workspace

---
+++ src/com/revature/zrin/util/List.java
@@ -0,0 +1,42 @@
+package com.revature.zrin.util;
+
+/**
+ * Interfaces
+ *
+ *      - acts as a "contract" for implementing classes (layman's terms: any concrete class that implements
+ *        this interface will be required to provide logic for the declared methods of this interface)
+ *
+*       - do not have constructors (and therefore cannot be instantiated)
+ *
+ *       - the interface declaration is implicitly abstract
+ *
+ *       - all method stubs declared within an interface are implicitly abstract and public
+ *
+ *       - can contain concrete (non-abstract) methods:
+ *          + static methods
+ *          + default methods (introduced in Java 8)
+ */
+public interface List<T> extends Collection<T> {
+
+    // the line below is an example of a method stub (a method without an implementation; can only be declared
+    // within an abstract construct (abstract class or interface)
+    T get(int index);
+
+    // Default methods are interface methods that have a default implementation
+    // Introduced in Java v8 (added to help to maintain backward compatibility)
+    // Can be overridden
+    default void defaultMethodExample() {
+        System.out.println("This is a default method in an interface, it CAN be overridden by implementing classes");
+    }
+
+    // Static methods
+    // Since Java 1
+    // Cannot be overridden
+    // Accessed like this: List.staticMethodExample()
+    static void staticMethodExample() {
+        System.out.println("This is a static method in an interface, it CANNOT be overridden - but it can be shadowed.");
+    }
+
+
+
+}
