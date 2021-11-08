2a76b361679cac6c1da8a24e22d2fa49d167d4c2
cjsssusa <cjsssusa@gmail.com>
2021-11-08 16:47:07 -0500

    read File in authenticateUser

--- src/com/revature/quizzard/services/UserService.java
+++ src/com/revature/quizzard/services/UserService.java
@@ -4,7 +4,9 @@ import com.revature.quizzard.daos.AppUserDAO;
 import com.revature.quizzard.exceptions.InvalidRequestException;
 import com.revature.quizzard.models.AppUser;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 
 public class UserService {
@@ -29,7 +31,17 @@ public class UserService {
     public AppUser authenticateUser(String username, String password) {
         File usersFile = new File("resources/data.txt");
 
+        try(BufferedReader br = new BufferedReader(new FileReader(usersFile))) {
+            for(String line; (line = br.readLine()) != null; ) {
 
+                String[] each = line.split(":");
+                if( each[4].equals(username) && each[5].equals(password)){
+                    //return appuser;
+                    return
+                }
+            }
+            return null;
+        }
         return null;
     }
 
