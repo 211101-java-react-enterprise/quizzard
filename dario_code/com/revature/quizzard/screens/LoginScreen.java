7e36cd0c0de9afcb0f3de4f21e3dc07001f4c617
dario robinson <dariorobinson@darios-air.lan>
2021-11-11 06:51:41 -0800

    Added some login screen features

--- dario_code/com/revature/quizzard/screens/LoginScreen.java
+++ dario_code/com/revature/quizzard/screens/LoginScreen.java
@@ -1,4 +1,28 @@
 package com.revature.quizzard.screens;
 
-public class LoginScreen {
+public class LoginScreen extends Screen {
+
+    private final UserService userService;
+
+    public LoginScreen(BufferedReader consoleReader, ScreenRouter router, UserService userService,) {
+        super("LoginScreen", "/login", consoleReader, router);
+        this.userService = userService;
+    }
+
+    @Override
+    public void render() throws Exception {
+        System.out.println("The user selected Login");
+
+        System.out.println("Please provide us with your login information.");
+
+        System.out.print("Username:");
+        String firstName = consoleReader.readLine();
+
+        System.out.print("Password:");
+        String lastName = consoleReader.readLine();
+
+
+
+
+
 }
