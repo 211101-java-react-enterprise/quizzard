dc88ad9014b7df43e87b7c671e43b4df2d2bbadc
wsingleton <wezley.singleton@gmail.com>
2021-11-11 22:44:50 -0500

    updated table-creation script to include new check constraints and flashcards table

--- src/main/resources/table-creation.sql
+++ src/main/resources/table-creation.sql
@@ -1,13 +1,29 @@
+drop table if exists flashcards;
+drop table if exists app_users;
+
 create table app_users (
-	id varchar,
-	first_name varchar(25) not null,
-	last_name varchar(25) not null,
-	email varchar(255) unique not null,
-	username varchar(20) unique not null,
-	
-	constraint app_users_pk 
-	primary key (id)
+    user_id varchar check (user_id <> ''),
+    first_name varchar(25) not null check (first_name <> ''),
+    last_name varchar(25) not null check (last_name <> ''),
+    email varchar(255) unique not null check (email <> ''),
+    username varchar(20) unique not null check (username <> ''),
+    password varchar(255) not null check (password <> ''),
+
+    constraint app_users_pk
+    primary key (user_id)
 );
 
-alter table app_users 
-add column password varchar not null;
+create table flashcards (
+    card_id varchar check (card_id <> ''),
+    question_text varchar not null check (question_text <> ''),
+    answer_text varchar not null check (answer_text <> ''),
+    creator_id varchar not null check (creator_id <> ''),
+
+    constraint flashcards_pk
+    primary key (card_id),
+
+    constraint flashcard_creator_fk
+    foreign key (creator_id)
+    references app_users
+);
+
