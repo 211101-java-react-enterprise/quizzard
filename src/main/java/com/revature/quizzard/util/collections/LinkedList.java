b2a64a34e86832b8d205ac667aec5339edde8e7e
wsingleton <wezley.singleton@gmail.com>
2021-11-15 00:16:10 -0500

    added boilerplate for new List methods to LinkedList

--- src/main/java/com/revature/quizzard/util/collections/LinkedList.java
+++ src/main/java/com/revature/quizzard/util/collections/LinkedList.java
@@ -1,11 +1,23 @@
 package com.revature.quizzard.util.collections;
 
+/**
+ * Singly-linked list implementation of the List and Deque interfaces. Permits
+ * all elements (including null).
+ *
+ * @param <T> the type of elements maintained by this list
+ */
 public class LinkedList<T> implements List<T> {
 
     private int size;
     private Node<T> head; // implicitly null
     private Node<T> tail = null; // you can explicitly declare them as null, but it's not required.
 
+    /**
+     * Appends the specified element to the end of this list.
+     *
+     * @param data element to be appended to this list
+     * @return true
+     */
     @Override
     public boolean add(T data) {
 
@@ -27,6 +39,14 @@ public class LinkedList<T> implements List<T> {
 
     }
 
+    /**
+     * Returns true if this list contains the specified element. More formally,
+     * returns true if and only if this list contains at least one element e
+     * such that (o==null ? e==null : o.equals(e)).
+     *
+     * @param data element whose presence in this list is to be tested
+     * @return true if this list contains the specified element
+     */
     @Override
     public boolean contains(T data) {
 
@@ -42,11 +62,25 @@ public class LinkedList<T> implements List<T> {
         return false;
     }
 
+    /**
+     * Returns true if this list contains no elements.
+     *
+     * @return true if this list contains no elements
+     */
     @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /**
+     * Removes the first occurrence of the specified element from this list, if it is present.
+     * If this list does not contain the element, it is unchanged. More formally, removes the
+     * element with the lowest index i such that (o==null ? get(i)==null : o.equals(get(i)))
+     * (if such an element exists). Returns true if this list contained the specified element.
+     *
+     * @param data element to be removed from this list, if present
+     * @return true if this list contained the specified element
+     */
     @Override
     public boolean remove(T data) {
 
@@ -88,16 +122,27 @@ public class LinkedList<T> implements List<T> {
         return false;
     }
 
+    /**
+     * Returns the number of elements in this list.
+     *
+     * @return the number of elements in this list
+     */
     @Override
     public int size() {
         return size;
     }
 
-    // TODO: IMPLEMENT ME!
+    /**
+     * Returns the element at the specified position in this list.
+     *
+     * @param index index of the element to return
+     * @return the element at the specified position in this list
+     * @throws IndexOutOfBoundsException if the index is out of range (index < 0 || index >= size())
+     */
     @Override
     public T get(int index) {
         if (index < 0 || index >= size) {
-            throw new RuntimeException("Provided index is out of bounds");
+            throw new IndexOutOfBoundsException("Provided index is out of bounds");
         }
 
         Node<T> currentNode = head;
@@ -109,6 +154,68 @@ public class LinkedList<T> implements List<T> {
         }
 
         return null;
+    }
+
+    @Override
+    public void add(int index, T element) {
+
+    }
+
+    /**
+     * Replaces the element at the specified position in this list with the
+     * specified element (optional operation).
+     *
+     * @param index index of the element to replace
+     * @param element element to be stored at the specified position
+     * @return the element previously at the specified position
+     */
+    @Override
+    public T set(int index, T element) {
+        return null;
+    }
+
+    /**
+     * Removes the element at the specified position in this list. Shifts any
+     * subsequent elements to the left (subtracts one from their indices).
+     * Returns the element that was removed from the list.
+     *
+     * @param index the index of the element to be removed
+     * @return the element previously at the specified position
+     * @throws IndexOutOfBoundsException if the index is out of range (index < 0 || index >= size())
+     */
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    /**
+     * Returns the index of the first occurrence of the specified element in this list,
+     * or -1 if this list does not contain the element. More formally, returns the lowest
+     * index i such that (o==null ? get(i)==null : o.equals(get(i))), or -1 if there is
+     * no such index.
+     *
+     * @param element element to search for
+     * @return the index of the first occurrence of the specified element in this list,
+     *         or -1 if this list does not contain the element
+     */
+    @Override
+    public int indexOf(T element) {
+        return -1;
+    }
+
+    /**
+     * Returns the index of the last occurrence of the specified element in this list,
+     * or -1 if this list does not contain the element. More formally, returns the highest
+     * index i such that (o==null ? get(i)==null : o.equals(get(i))), or -1 if there is
+     * no such index.
+     *
+     * @param element element to search for
+     * @return the index of the last occurrence of the specified element in this list,
+ *             or -1 if this list does not contain the element
+     */
+    @Override
+    public int lastIndexOf(T element) {
+        return -1;
     }
 
     @Override
@@ -134,16 +241,6 @@ public class LinkedList<T> implements List<T> {
         strBldr.append("]");
 
         return strBldr.toString();
-    }
-
-    //    @Override // works without issue!
-//    public void defaultMethodExample() {
-//        System.out.println("This is an overridden default method.");
-//    }
-
-//    @Override // static methods inherited from interfaces CANNOT be overridden; but
-    public static void staticMethodExample() {
-        System.out.println("This is a method that shadows the static method declared by List");
     }
 
     // Nested Inner Class
