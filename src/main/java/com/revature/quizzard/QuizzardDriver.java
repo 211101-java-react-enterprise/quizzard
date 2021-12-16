f837a6445cef53b4f4d85dedabad64f9fbe60d38
Wezley Singleton <wezley.singleton@gmail.com>
2021-12-16 00:44:00 -0500

    added swagger/openapi

--- src/main/java/com/revature/quizzard/QuizzardDriver.java
+++ src/main/java/com/revature/quizzard/QuizzardDriver.java
@@ -2,6 +2,11 @@ package com.revature.quizzard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class QuizzardDriver {
@@ -10,4 +15,12 @@ public class QuizzardDriver {
         SpringApplication.run(QuizzardDriver.class, args);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
